327576586b2e2002eff3f8df0fb0f49f4bf06ead
Gergő Erős <eros.gergo@codeyard.eu>
2021-09-29 19:43:31 +0200

    deploy

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MainScreen/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainScreen from './views/MainScreen.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainScreen
   }
 }
 </script>
